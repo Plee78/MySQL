3d13b407b7ff6ac4045eec959d48ecf2da2547f3
Patrick Lee <125105019+Plee78@users.noreply.github.com>
2023-04-25 22:43:15 -0500

    Add files via upload

---
+++ Grouping By Multiple Columns.sql
@@ -0,0 +1,13 @@
+-- Grouping by multiple columns
+
+SELECT 
+    author_fname, author_lname, COUNT(*)
+FROM
+    books
+GROUP BY author_lname , author_fname;
+
+SELECT 
+    CONCAT(author_fname, ' ', author_lname) AS author, COUNT(*)
+FROM
+    books
+GROUP BY author;
